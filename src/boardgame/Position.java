e1a676e21b70926662481cbc653d1efe3c35dff0
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2023-11-17 10:33:58 -0300

    Starting to implement Board and Piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -30,10 +30,4 @@ public class Position {
     public String toString() {
         return row + ", " + column;
     }
-
-    public void setValues(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
 }
