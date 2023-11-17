ab421f7d02b15911b35300f7b57ec6576e3b0a49
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2023-11-17 14:04:43 -0300

    Possible moves of a piece

--- chess-system/src/applications/Program.java
+++ chess-system/src/applications/Program.java
@@ -24,7 +24,7 @@ public class Program {
                 ChessPosition source = UI.readChessPosition(sc);
 
                 System.out.println();
-                System.out.print("Source: ");
+                System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
